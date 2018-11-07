2d0a76b937f26531230c46cbef3a4ae650e87af9
wuchen <wuchen@yyhudong.com>
2018-11-07 14:25:12 +0800

    gomvp-plugin 1.0.1

--- gomvp-plugin/src/main/groovy/com/gomvp/gradle/plugin/DependenciesPlugin.groovy
+++ gomvp-plugin/src/main/groovy/com/gomvp/gradle/plugin/DependenciesPlugin.groovy
@@ -10,15 +10,5 @@ public class DependenciesPlugin implements Plugin<Project> {
         project.extensions.create('libs', Libs)
         project.extensions.create('Versions', Version)
         project.extensions.create('Configs', Config)
-
-        project.extensions.dds = '123'
-        //这里实现plugin的逻辑
-        //巴拉巴拉巴拉
-        println "hello, this is cooker plugin!"
-        //cooker-plugin
-        //比如这里加一个简单的task
-        project.task('cooker-test-task') << {
-            println deps.libs.okhttp
-        }
     }
 }
